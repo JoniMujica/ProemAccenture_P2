255b69ce31580dcaf8d0d0a6a69b0eee636c74ca
JoniMujica <jonimujica3@hotmail.com>
2021-11-17 15:00:12 -0300

    POO Clase 25

---
+++ Objetos/BibliotecaDeClases/Mascota.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BibliotecaDeClases
+{
+    public class Mascota
+    {
+        //areibutos
+        private string nombre;
+        private int edad;
+
+        public static string saludo;
+        
+        static Mascota() //static se usa para una entidad que tendra el mismo valor para todos los objetos, se puede decir que se usa para inicializar un onjeto
+        {
+            Mascota.saludo = "Hola";
+        }
+
+        //constructor
+        public Mascota(string nombre,int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        //metodos
+
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string MostrarDatos()
+        {
+            return $"{Mascota.saludo} mi nombres es {nombre} y tengo {edad} años";
+        }
+    }
+}
---
+++ Objetos/Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using BibliotecaDeClases;
+using System;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mascota mascota = new Mascota("lucy",21);
+
+            //Console.WriteLine(Mascota.saludo);
+            // Console.WriteLine(mascota.MostrarDatos());
+            Console.WriteLine(mascota.GetNombre());
+            mascota.SetNombre("Mia");
+            Console.WriteLine(mascota.GetNombre());
+        }
+    }
+}
