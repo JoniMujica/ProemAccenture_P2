d301b06a51f740bce85414d3acde659758e1a2c4
JoniMujica <jonimujica3@hotmail.com>
2021-11-17 08:36:15 -0300

    se realizo en clase la tabla de multiplicar EJ1

---
+++ Clases_y_Metodos_Estaticos/Biblioteca/Multiplicacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class Multiplicacion
+    {
+        public static string Multiplicar(int numero)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Tabla de multiplicar del numero {0}{1}", numero,Environment.NewLine);
+            for (int i = 1; i <= 10; i++)
+            {
+                sb.AppendFormat("{0} x {1} = {2}{3}", numero, i, numero * i,Environment.NewLine);
+            }
+            return sb.ToString();
+        }
+    }
+}
--- Clases_y_Metodos_Estaticos/Clases_y_Metodos_Estaticos/Program.cs
+++ Clases_y_Metodos_Estaticos/Clases_y_Metodos_Estaticos/Program.cs
@@ -1,4 +1,5 @@
-﻿using BibliotecaDeClases;
+﻿using Biblioteca;
+using BibliotecaDeClases;
 using System;
 // using SC = System.Console; //ALIAS
 
@@ -9,10 +10,36 @@ namespace Clases_y_Metodos_Estaticos
         static void Main(string[] args)
         {
             // SC.WriteLine("Hello World!"); //uso de ALIAS
+
+            /*
             Cotizador.CotizarVentaDolares(45.2M);
 
             string texto = Cotizador.MostrarDatos();
             Console.WriteLine(texto);
+
+            */
+
+            ///ej 1
+            /*
+            int numero = 0;
+            string tabla;
+            bool CNV = false;
+            do
+            {
+                Console.WriteLine("Ingrese un numero para hacer su tabla de multiplicacion: ");
+                CNV = Int32.TryParse(Console.ReadLine(), out numero);
+                if (CNV)
+                {
+                    tabla = Multiplicacion.Multiplicar(numero);
+                    Console.WriteLine(tabla);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: el numero ingresado es invalido, por favor ingrese un valor numerico entero");
+                }
+            } while (!CNV);
+            */
+
         }
     }
 }
