334914e58126fbcfc70f0ed9953688abd0adec74
JoniMujica <jonimujica3@hotmail.com>
2021-11-17 18:49:28 -0300

    Termine el EJ2

---
+++ Objetos EJS/Consola/Ej2/Program.cs
@@ -0,0 +1,26 @@
+﻿using BibliotecaDeClases;
+using System;
+
+namespace Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante est1 = new Estudiante("Joni","Mujica","204377-1");
+            Estudiante est2 = new Estudiante("Javi", "escalada", "48342-4");
+            Estudiante est3 = new Estudiante("Yamila", "Jurado", "196751-7");
+
+            est1.SetNotaPrimerParcial(7);
+            est1.SetNotaSegundoParcial(8);
+            est2.SetNotaPrimerParcial(5);
+            est2.SetNotaSegundoParcial(3);
+            est3.SetNotaPrimerParcial(8);
+            est3.SetNotaSegundoParcial(9);
+
+            Console.WriteLine(est1.Mostrar());
+            Console.WriteLine(est2.Mostrar());
+            Console.WriteLine(est3.Mostrar());
+        }
+    }
+}
---
+++ Objetos EJS/EJ1/EJ1/Program.cs
@@ -0,0 +1,18 @@
+﻿using BibliotecaDeClases;
+using System;
+
+namespace EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta = new Cuenta("Joni",25000);
+            Console.WriteLine(cuenta.Mostrar());
+            cuenta.Ingresar(3000);
+            Console.WriteLine(cuenta.Mostrar());
+            cuenta.Retirar(30000);
+            Console.WriteLine(cuenta.Mostrar());
+        }
+    }
+}
