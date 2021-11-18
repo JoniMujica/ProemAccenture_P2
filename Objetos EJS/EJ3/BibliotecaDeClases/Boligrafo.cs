53f290db708937c6f43ee9209f2c1dcbb8c6d462
JoniMujica <jonimujica3@hotmail.com>
2021-11-17 21:48:32 -0300

    Termine el ejercicio 3 excepto ConsoleColor en el print del boligrafo de color X

---
+++ Objetos EJS/EJ3/BibliotecaDeClases/Boligrafo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BibliotecaDeClases
+{
+    public class Boligrafo
+    {
+        private short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            short dif = (short)(this.tinta - gasto);
+            int aux = 0;
+            string print = "";
+            if (this.tinta > 0)
+            {
+                if (dif > 0)
+                {
+                    aux = gasto;
+                    SetTinta((short)(-1*gasto));
+                }
+                else if (dif <= 0)
+                {
+                    aux = this.tinta;
+                    SetTinta((short)(-1 * this.tinta));
+                }
+
+                for (int i = 0; i < aux; i++)
+                {
+                    print += '*'.ToString();
+                }
+                dibujo = print;
+                //SetTinta((short)(this.tinta - gasto));
+                return true;
+            }
+            else
+            {
+                dibujo = "";
+                return false;
+            }
+
+        }
+        public void Recargar()
+        {
+            SetTinta((short)(cantidadTintaMaxima - this.tinta));
+        }
+        private void SetTinta(short tinta)
+        {
+            if (tinta <= 0)
+            {
+                if (this.tinta + tinta >= 0)
+                {
+                    this.tinta += tinta;
+                }
+            }
+            if (tinta > 0)
+            {
+                if (this.tinta + tinta <= cantidadTintaMaxima)
+                {
+                    this.tinta += tinta;
+                }
+            }
+        }
+    }
+}
---
+++ Objetos EJS/EJ3/EJ3/Program.cs
@@ -0,0 +1,31 @@
+﻿using BibliotecaDeClases;
+using System;
+
+namespace EJ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dib1,dib2;
+            Boligrafo bol1 = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo bol2 = new Boligrafo(50, ConsoleColor.Red);
+            Console.WriteLine(bol1.GetColor());
+            Console.WriteLine(bol2.GetColor());
+            Console.WriteLine(bol1.GetTinta());
+            Console.WriteLine(bol2.GetTinta());
+            bool res1 = bol1.Pintar(70,out dib1);
+            bool res2 = bol2.Pintar(50, out dib2);
+            Console.WriteLine(dib1);
+            Console.WriteLine(dib2);
+            Console.WriteLine(res2);
+            Console.WriteLine(bol2.GetTinta());
+            //bol2.Recargar();
+            Console.WriteLine(bol2.GetTinta());
+            res2 = bol2.Pintar(10, out dib2);
+            Console.WriteLine(dib2);
+            Console.WriteLine(res2);
+            Console.WriteLine(bol2.GetTinta());
+        }
+    }
+}
