93ed6e308b0e3f766df96d70ae65e86c3ccdba11
JoniMujica <jonimujica3@hotmail.com>
2021-12-05 18:37:44 -0300

    Herencia

---
+++ Herencia/Herencia/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("Tomas", 20);
+            Jugador jugador = new Jugador("Chacarita","Carpincho Malvado",20);
+            Persona jugador2 = new Jugador("HLH", "Carpincho Malvado", 20);
+
+            Console.WriteLine($"Persona: {persona.Mostrar()}");
+            Console.WriteLine($"Jugador: {jugador.MostrarJugador()}");
+            Console.WriteLine($"Jugador2: {((Jugador)jugador2).MostrarJugador()}");
+        }
+    }
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+        private string direccion;
+        public Persona(string nombre,int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        
+        public string Mostrar()
+        {
+            return $"Nombre {nombre} - Edad {edad}";
+        }
+    }
+    public class Jugador : Persona
+    {
+        string equipo;
+        public Jugador(string equipo,string nombre,int edad):base(nombre,edad)
+        {
+            this.equipo = equipo;
+        }
+        public string MostrarJugador()
+        {
+            return $"{base.Mostrar()} - Equipo {equipo}";
+        }
+
+    }
+}
