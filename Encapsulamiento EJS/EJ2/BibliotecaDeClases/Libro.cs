5200e997c63ce071f2743e143cae5cf1798f0d95
JoniMujica <jonimujica3@hotmail.com>
2021-12-05 18:00:45 -0300

    EJ2 Sin Terminar

---
+++ Encapsulamiento EJS/EJ2/BibliotecaDeClases/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibliotecaDeClases
+{
+    public class Libro
+    {
+        private List<string> paginas;
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+
+    }
+}
