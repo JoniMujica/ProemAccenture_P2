83a7ab281470f7a5b5e656f7b8db3007c2a76ce6
JoniMujica <jonimujica3@hotmail.com>
2021-12-05 13:31:01 -0300

    Termine ejercicio EJ1 Encapsulamiento

---
+++ Encapsulamiento EJS/EJ1/BibliotecaDeClases/PuestoAtencion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClases
+{
+    public class PuestoAtencion
+    {
+        private static int numeroActual;
+        private Puesto puesto;
+
+
+        public int NumeroActual
+        {
+            get
+            {
+                return numeroActual;
+            }
+            set
+            {
+                numeroActual++;
+            }
+        }
+        public bool Atender(Cliente c)
+        {
+            Thread.Sleep(900);
+            return true;
+        }
+        public PuestoAtencion()
+        {
+            numeroActual = 0;
+        }
+        public PuestoAtencion(Puesto puesto):this()
+        {
+            this.puesto = puesto;
+        }
+
+        public enum Puesto
+        {
+            Caja1,
+            Caja2
+        }
+    }
+}
---
+++ Encapsulamiento EJS/EJ1/EJ1/Program.cs
@@ -0,0 +1,38 @@
+﻿using BibliotecaDeClases;
+using System;
+
+namespace EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Cliente c1 = new Cliente(1, "Tomas");
+            Cliente c2 = new Cliente(2, "Paula");
+            Cliente c3 = new Cliente(3, "Esteban");
+            Negocio n1 = new Negocio("Kiosco-24");
+            if (n1 + c1)
+            {
+                Console.WriteLine("Se agrego c1 a la cola");
+            }
+            if (!(n1 + c1))
+            {
+                Console.WriteLine("Cliente c1 ya esta en la cola");
+            }
+            if (n1 + c2)
+            {
+                Console.WriteLine("Se agrego c2 a la cola");
+            }
+            if (n1 + c3)
+            {
+                Console.WriteLine("Se agrego c3 a la cola");
+            }
+            Console.WriteLine("Clientes pendientes: {0}", n1.ClientesPendientes);
+            while (~n1)
+            {
+                Console.WriteLine("Clientes pendientes: {0}", n1.ClientesPendientes);
+            }
+        }
+    }
+}
