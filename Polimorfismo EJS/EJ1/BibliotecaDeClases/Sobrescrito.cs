1cee3ac9f409406cef3296d0e0464673b404471f
JoniMujica <jonimujica3@hotmail.com>
2021-12-06 11:07:27 -0300

    FIN EJ1

---
+++ Polimorfismo EJS/EJ1/BibliotecaDeClases/Sobrescrito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BibliotecaDeClases
+{
+    public class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi metodo ToString sobreescrito";
+        }
+        public override bool Equals(object obj)
+        {
+            //return obj is not null && this.GetType() == obj.GetType();
+            return obj is not null && obj.GetType() == typeof(Sobreescrito);
+        }
+        public override int GetHashCode()
+        {
+            return 1142510181;
+        }
+
+    }
+}
---
+++ Polimorfismo EJS/EJ1/EJ1/Program.cs
@@ -0,0 +1,21 @@
+﻿using BibliotecaDeClases;
+using System;
+
+namespace EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Sobre-Sobrescrito";
+            Sobreescrito objetoSobrescrito = new Sobreescrito();
+            Console.WriteLine(objetoSobrescrito.ToString());
+            string objeto = "¡Este es mi método ToString sobrescrito!";
+            Console.WriteLine("----------------------------------------------");
+            Console.Write("Comparación Sobrecargas con String: ");
+            Console.WriteLine(objetoSobrescrito.Equals(objeto));
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(objetoSobrescrito.GetHashCode());
+        }
+    }
+}
