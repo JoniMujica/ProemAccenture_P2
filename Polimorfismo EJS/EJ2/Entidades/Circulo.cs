ba7280c52ef3ee4f06993667bf4eade471d9bb5b
JoniMujica <jonimujica3@hotmail.com>
2021-12-06 18:27:39 -0300

    Termine EJ2 (Ayuda prof

---
+++ Polimorfismo EJS/EJ2/Entidades/Circulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public sealed class  Circulo : Figura
+    {
+        private float radio;
+        public Circulo(float radio)
+        {
+            this.radio = radio;
+        }
+        public override double CaulcularPerimetro()
+        {
+            return Math.PI * this.radio * 2;
+        }
+
+        public override double CaulcularSuperficie()
+        {
+            return Math.PI * Math.Pow(this.radio, 2);
+        }
+        public override string Dibujar()
+        {
+            return "Dibujando un circulo.....";
+        }
+    }
+}
