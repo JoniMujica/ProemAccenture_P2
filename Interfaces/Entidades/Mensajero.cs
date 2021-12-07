d00c037f616f13e84df1ac429823333c87e3e701
JoniMujica <jonimujica3@hotmail.com>
2021-12-07 13:32:59 -0300

    Clase interfases

---
+++ Interfaces/Entidades/Mensajero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Mensajero : Persona, IMensajero
+    {
+        public void EnviarMensaje()
+        {
+            Console.WriteLine("Soy el mensajero y traigo un mensaje");
+        }
+    }
+}
---
+++ Interfaces/Entidades/Paloma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Paloma : IMensajero
+    {
+        public int count
+        {
+            set
+            {
+
+            }
+        }
+
+        public void EnviarMensaje()
+        {
+            Console.WriteLine("Soy una paloma mensajera y traigo mensaje");
+        }
+    }
+}
